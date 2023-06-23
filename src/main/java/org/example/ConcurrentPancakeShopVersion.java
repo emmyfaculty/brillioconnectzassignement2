1c663b806b8963bd7b2b3236f9d2ae0390cfb062
Ahola Emmanuel <emmanuelahola91@gmail.com>
2023-06-23 13:38:58 +0100

    second aspect of the assignment from brillioConnectz

--- src/main/java/org/example/ConcurrentPancakeShopVersion.java
+++ src/main/java/org/example/ConcurrentPancakeShopVersion.java
@@ -10,31 +10,23 @@ public class ConcurrentPancakeShopVersion {
     private static final int MAX_PANCAKES_PER_SLOT = 12;
     private static final int NUM_USERS = 3;
     private static final int TOTAL_TIME_SECONDS = 30;
-
     private Random random;
-
     public ConcurrentPancakeShopVersion() {
         random = new Random();
     }
-
     public void servePancakes() {
         Instant startTime = Instant.now();
         Instant endTime = startTime.plusSeconds(TOTAL_TIME_SECONDS);
-
         int totalPancakesRequested = 0;
         final int[] totalPancakesServed = {0};
-
         int[] pancakesRequested = new int[NUM_USERS];
         int[] pancakesServed = new int[NUM_USERS];
-
         // Generate pancake requests for each user
         for (int i = 0; i < NUM_USERS; i++) {
             pancakesRequested[i] = random.nextInt(MAX_PANCAKES_PER_USER + 1);
             totalPancakesRequested += pancakesRequested[i];
         }
-
         CompletableFuture<Void>[] completableFutures = new CompletableFuture[NUM_USERS];
-
         // Serve pancakes to users concurrently using CompletableFuture
         for (int i = 0; i < NUM_USERS; i++) {
             int userIndex = i;
@@ -50,13 +42,11 @@ public class ConcurrentPancakeShopVersion {
                 }
             });
         }
-
         try {
             CompletableFuture.allOf(completableFutures).get();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
-
         int pancakesWasted = totalPancakesServed[0] - totalPancakesRequested;
         int unmetOrders = 0;
         for (int i = 0; i < NUM_USERS; i++) {
@@ -64,7 +54,6 @@ public class ConcurrentPancakeShopVersion {
                 unmetOrders++;
             }
         }
-
         // Output the results
         System.out.println("Start Time: " + startTime);
         System.out.println("End Time: " + endTime);
@@ -72,7 +61,6 @@ public class ConcurrentPancakeShopVersion {
         System.out.println("Pancakes wasted: " + pancakesWasted);
         System.out.println("Pancake orders not met: " + unmetOrders);
     }
-
     public static void main(String[] args) {
         ConcurrentPancakeShopVersion pancakeShop = new ConcurrentPancakeShopVersion();
         pancakeShop.servePancakes();
--- src/main/java/org/example/NonCuncurrentPancakeShopVersion.java
+++ src/main/java/org/example/NonCuncurrentPancakeShopVersion.java
@@ -2,35 +2,27 @@ package org.example;
 
 import java.time.Instant;
 import java.util.Random;
-
 public class NonCuncurrentPancakeShopVersion {
     private static final int MAX_PANCAKES_PER_USER = 5;
     private static final int MAX_PANCAKES_PER_SLOT = 12;
     private static final int NUM_USERS = 3;
     private static final int TOTAL_TIME_SECONDS = 30;
-
     private Random random;
-
     public NonCuncurrentPancakeShopVersion() {
         random = new Random();
     }
-
     public void servePancakes() {
         Instant startTime = Instant.now();
         Instant endTime = startTime.plusSeconds(TOTAL_TIME_SECONDS);
-
         int totalPancakesRequested = 0;
         int totalPancakesServed = 0;
-
         int[] pancakesRequested = new int[NUM_USERS];
         int[] pancakesServed = new int[NUM_USERS];
-
         // Generate pancake requests for each user
         for (int i = 0; i < NUM_USERS; i++) {
             pancakesRequested[i] = random.nextInt(MAX_PANCAKES_PER_USER + 1);
             totalPancakesRequested += pancakesRequested[i];
         }
-
         // Serve pancakes to users
         for (int i = 0; i < NUM_USERS; i++) {
             int pancakesToServe = Math.min(MAX_PANCAKES_PER_USER, pancakesRequested[i]);
@@ -41,7 +33,6 @@ public class NonCuncurrentPancakeShopVersion {
                 pancakesServed[i] = 0;
             }
         }
-
         int pancakesWasted = totalPancakesServed - totalPancakesRequested;
         int unmetOrders = 0;
         for (int i = 0; i < NUM_USERS; i++) {
@@ -49,7 +40,6 @@ public class NonCuncurrentPancakeShopVersion {
                 unmetOrders++;
             }
         }
-
         // Output the results
         System.out.println("Start Time: " + startTime);
         System.out.println("End Time: " + endTime);
@@ -57,7 +47,6 @@ public class NonCuncurrentPancakeShopVersion {
         System.out.println("Pancakes wasted: " + pancakesWasted);
         System.out.println("Pancake orders not met: " + unmetOrders);
     }
-
     public static void main(String[] args) {
         NonCuncurrentPancakeShopVersion pancakeShop = new NonCuncurrentPancakeShopVersion();
         pancakeShop.servePancakes();
